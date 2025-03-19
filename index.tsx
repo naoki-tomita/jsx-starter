5a52de5a32e043817bf30fefa5d8cd3a71d5bdb5
naoki-tomita <mxcpy608@hotmail.com>
2025-03-19 22:47:12 +0900

    できた

--- index.tsx
+++ index.tsx
@@ -5,7 +5,10 @@ type MyElement = {
 }
 
 const React = {
-  createElement(tag: string, attributes: Record<string, any>, ...children: MyElement[]): MyElement {
+  createElement(tag: string | FunctionComponent, attributes: Record<string, any>, ...children: MyElement[]): MyElement {
+    if (typeof tag === 'function') {
+      return tag(attributes ?? {}, children);
+    }
     return {
       tag,
       attributes: attributes ?? {},
@@ -13,6 +16,8 @@ const React = {
     }
   }
 }
+
+type FunctionComponent<T = {}> = (props: T, children: MyElement[]) => MyElement;
 
 function attributesToString(attributes: Record<string, any>) {
   return Object.entries(attributes).map(([key, value]) => ` ${key}="${value}"`).join('');
@@ -25,10 +30,24 @@ function render(element: MyElement) {
   return `<${element.tag}${attributesToString(element.attributes)}>${element.children.map(render).join('')}</${element.tag}>`;
 }
 
-const html = render(
-  <div class="foo bar">
-    <h1 data-testid="hoge">Hello World</h1>
-    <p>This is a paragraph</p>
-  </div>
-);
+const Component: FunctionComponent<{ foo: string, bar: string }> = ({ foo, bar }) => {
+  return (
+    <div>
+      <h2>{foo}</h2>
+      <p>{bar}</p>
+    </div>
+  )
+}
+
+const App: FunctionComponent = () => {
+  return (
+    <div class="foo bar">
+      <h1 data-testid="hoge">Hello World</h1>
+      <p>This is a paragraph</p>
+      <Component foo="Hello" bar="World" />
+    </div>
+  )
+}
+
+const html = render(<App />);
 console.log(html);
